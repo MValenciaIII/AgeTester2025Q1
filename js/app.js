40acde9a210cb57e6c4b7a0f90493551a0ff8d71
Margarito Valencia III <52177176+MValenciaIII@users.noreply.github.com>
2025-05-16 10:00:18 -0500

    Removed event listener code

--- js/app.js
+++ js/app.js
@@ -7,12 +7,5 @@ let displayResult = document.querySelector(".displayResultCol h3")
 console.log(displayResult);
 
 checkButton.addEventListener("click", () => {
-    let ageInput = document.getElementById("ageInput");
-    console.log(ageInput.value)
-    
-    if (ageInput.value >= 18) {
-        displayResult.innerText = "Come on in, You're old enough."
-    } else if (ageInput.value < 18 ) {
-        displayResult.innerText = "Get out here!"
-    }
+
 })
